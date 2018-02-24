f9ccbffbaaa2e5faa316acce2ce50650c7a454e3
Arnout Boks <arnoutboks@gmail.com>
2018-02-24 14:49:40 +0100

    Add implementation of L1Norm

---
+++ src/Norm/L1Norm.php
@@ -0,0 +1,11 @@
+<?php
+namespace Aboks\PowerIteration\Norm;
+
+use Aboks\PowerIteration\Norm;
+use MathPHP\LinearAlgebra\Vector;
+
+class L1Norm implements Norm {
+	public function normOf(Vector $vector): float {
+		return $vector->l1Norm();
+	}
+}
---
+++ test/Norm/L1NormTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Aboks\PowerIteration\Tests\Norm;
+
+use Aboks\PowerIteration\Norm\L1Norm;
+use MathPHP\LinearAlgebra\Vector;
+use PHPUnit\Framework\TestCase;
+
+class L1NormTest extends TestCase {
+	/** @var L1Norm */
+	private $l1norm;
+
+	protected function setUp() {
+		$this->l1norm = new L1Norm();
+	}
+
+	public function testReturnsTheL2NormOfAVector() {
+		$v = new Vector([1, -1]);
+		$this->assertEquals(2, $this->l1norm->normOf($v));
+	}
+}
